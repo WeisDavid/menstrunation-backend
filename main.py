4090fe5b3fdef6bfdcd9feb9688a72c38bae1157
David Weis <david.weis@vema-eg.de>
2025-04-02 12:34:50 +0200

    statisches Antwortobjekt erstellt Einfache testmethode erstellt

--- main.py
+++ main.py
@@ -2,7 +2,25 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+testUser = {
+    "id": "55",
+    "userName": "testman",
+    "pw": "wawa123",
+    "email": "testmail@test.com",
+    "alter": 22,
+    "gewicht": 80,
+    "größe": 181
+}
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/user/getById/{userId}")
+async def getUserById(userId: str):
+    if userId == "55":
+        return testUser
+    else:
+        return {"message": "User with ID " + userId + " does not exist!"}
+
+
