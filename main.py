b0dea580cacf1492c5106794a60971694f68b310
David Weis <david.weis@vema-eg.de>
2025-04-04 12:36:15 +0200

    - Request Model definiert - Methodengrundgerüst definiert

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from typing import Annotated
+from pydantic import BaseModel, Field, EmailStr
 
 from db import SessionDep, get_single_entity_by_id, create_single_entity_by_id, update_single_entity_by_id, delete_single_entity_by_id
 from models.users import User, CreateUser
 
 app = FastAPI()
 
+class UpdateRequest(BaseModel):
+    username: str | None = Field( default=None, title="Neuer Benutzername" )
+    pw: str | None = Field( default=None, title="Neues Passwort der Benutzers" )
+    email: EmailStr | None = Field( default=None, title="Neues Email des Benutzers" )
+    alter: int | None = Field( default=None, title="Neues Alter des Benutzers" )
+    gewicht: float | None = Field( default=None, title="Neues Gewicht des Benutzers" )
+    größe: float | None = Field( default=None, title="Neue Größe des Benutzers" )
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.put("/user/update/{user_id}")
+async def update_user(user_id: Annotated[ int, Query(title="Benutzer ID", description="ID des Benutzers") ], update_request: UpdateRequest):
+    return 1
