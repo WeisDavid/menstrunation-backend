0bd566a252c6bcf639b944559da8a3a1b552beb5
Markcggck <ma.rux@schueler.bs1-bt.de>
2025-04-04 12:39:37 +0200

    get und post und so

--- main.py
+++ main.py
@@ -1,8 +1,34 @@
 from fastapi import FastAPI
 
+class userIn:
+    username: str
+    email: str
+    alter: int
+    gewicht: float
+    groesse: float
+
+class userOut:
+    id: int
+
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
+    
     return {"message": "Hello World"}
+
+
+@app.get("/Philipp/")
+async def get_user(user_id):
+    id = 69
+    username = "Philipp"
+    email = "Philipp@KING.com"
+    alter = 69
+    gewicht = 69.69
+    groesse = 69.69 
+    return {  "id": id, "username": username, "email":email, "alter":alter, "gewicht":gewicht, "groesse":groesse}
+
+@app.post("/user/", response_model=userOut)
+async def create_user(user: userIn):
+    return user
