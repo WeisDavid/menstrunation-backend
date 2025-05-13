f01e5a5817b0f1aeccef5e68bf920f9c6fe825d3
GitHino <119617731+GitHino@users.noreply.github.com>
2025-05-13 10:46:26 +0200

    #16 platzhaltercheck ob der user existiert

--- main.py
+++ main.py
@@ -6,8 +6,12 @@ app = FastAPI()
 
 @app.get("/user_delete/{user_id}")
 async def user_delete(user_id: int):
-    if (user_id not NULL):    
+
+    user_exists = True
+    # in der wahren anwendung muss hier noch eine DB-Abfrage stattfinden, ob der User existiert mittels einer query
+
+    if user_exists::    
         delete = {"delete_message": "User deleted successfully"}
         return delete_message 
     else:
-        return {"User does not exist"}
+        raise HTTPException(status_code=404, detail=f"User with ID {user_id} not found")
