cabf3c5d7f57bad23831c832afe0503217024df9
Philipp <ph.herrmann@schueler.bs1-bt.de>
2025-05-13 11:32:07 +0200

    #12 Add db session

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 
+from db import SessionDep, get_single_entity_by_id, create_single_entity_by_id, update_single_entity_by_id, delete_single_entity_by_id
+from models.users import User, CreateUser
+
 app = FastAPI()
 
 
