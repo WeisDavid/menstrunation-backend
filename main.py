a4a19c851e5b4b9c4f078b04137e01ff601fcdbc
GitHino <119617731+GitHino@users.noreply.github.com>
2025-05-13 10:27:20 +0200

    #16 jetzt wird auch die message ausgegeben und nicht die dict

--- main.py
+++ main.py
@@ -10,4 +10,4 @@ app = FastAPI()
 @app.get("/user_delete/{user_id}")
 async def user_delete(user_id: int):    
     delete = {"delete_message": "User deleted successfully"}
-    return delete
+    return delete_message 
