a80e907d344eb28ee13bf1fc9f4b137664309eee
GitHino <119617731+GitHino@users.noreply.github.com>
2025-05-13 11:32:04 +0200

    #16 Jetzt Actually Final version, jetzt gibt es die Exception auch wirklich

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 from db import SessionDep, get_single_entity_by_id, create_single_entity_by_id, update_single_entity_by_id, delete_single_entity_by_id
 from models.users import User, CreateUser
@@ -10,7 +10,7 @@ app = FastAPI()
 @app.get("/user_delete/{user_id}")
 async def user_delete(user_id: int):
 
-    user_exists = True
+    user_exists = False
     # in der wahren anwendung muss hier noch eine DB-Abfrage stattfinden, ob der User existiert mittels einer query
 
     if user_exists:
