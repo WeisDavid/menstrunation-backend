2ead3f6c2233fd66109e01a0e59b1d4b5fad37e5
GitHino <119617731+GitHino@users.noreply.github.com>
2025-05-13 12:04:54 +0200

    #16 Updated user delete

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI, HTTPException
 
+from db import SessionDep, get_single_entity_by_id, create_single_entity_by_id, update_single_entity_by_id, delete_single_entity_by_id
+from models.users import User, CreateUser
+
 app = FastAPI()
 
 
@@ -7,11 +10,8 @@ app = FastAPI()
 @app.get("/user_delete/{user_id}")
 async def user_delete(user_id: int):
 
-    user_exists = False
-    # in der wahren anwendung muss hier noch eine DB-Abfrage stattfinden, ob der User existiert mittels einer query
-
-    if user_exists:
-        delete_message = "User deleted successfully"    
-        return delete_message 
-    else:
+    deleted = delete_single_entity_by_id(session, User, user_id)
+    if not deleted: 
         raise HTTPException(status_code=404, detail=f"User with ID {user_id} not found")
+    
+    return {"message": f"User with ID {user_id} deleted successfully."}
