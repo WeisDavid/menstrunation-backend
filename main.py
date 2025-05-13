3658038e41655f5d7a6821dfae3f204d9b057ca7
GitHino <119617731+GitHino@users.noreply.github.com>
2025-05-13 10:57:28 +0200

    #16 probiere grad wild rum mal schauen

--- main.py
+++ main.py
@@ -10,8 +10,9 @@ async def user_delete(user_id: int):
     user_exists = True
     # in der wahren anwendung muss hier noch eine DB-Abfrage stattfinden, ob der User existiert mittels einer query
 
-    if user_exists:    
-        delete = {"delete_message": "User deleted successfully"}
+    if user_exists:
+        delete_message = "User deleted successfully"    
+        #delete = {"delete_message": "User deleted successfully"}
         return delete_message 
     else:
         raise HTTPException(status_code=404, detail=f"User with ID {user_id} not found")
