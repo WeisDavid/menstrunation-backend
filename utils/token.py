2f7c68f9529522cee389ed48e958a7b33baeb0b0
Philipp <ph.herrmann@schueler.bs1-bt.de>
2025-05-27 11:47:30 +0200

    #22 Updated token utils

--- utils/token.py
+++ utils/token.py
@@ -7,10 +7,12 @@ from fastapi.security import OAuth2PasswordBearer
 from typing import Annotated
 
 from models.token import TokenData
+from models.user import UserInDB
 from db.users import get_single_user_by_username
+from db.session import SessionDep
 
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/user/login")
 
 
 SECRET_KEY = "ab955b6a302b5c2e5b28e3003611ed6a383054f7cbabab71fd3fae34500c87ae"
@@ -18,7 +20,7 @@ ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 
-def create_access_token(data: dict, expires_delta: timedelta | None = None):
+def create_access_token(data: dict, expires_delta: timedelta | None = None) -> str:
     to_encode = data.copy()
     if expires_delta:
         expire = datetime.now(timezone.utc) + expires_delta
@@ -29,7 +31,7 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
     return encoded_jwt
 
 
-async def get_current_user(session, token: Annotated[str, Depends(oauth2_scheme)]):
+async def get_current_user(session: SessionDep, token: Annotated[str, Depends(oauth2_scheme)]) -> UserInDB:
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
@@ -43,7 +45,7 @@ async def get_current_user(session, token: Annotated[str, Depends(oauth2_scheme)
         token_data = TokenData(username=username)
     except InvalidTokenError:
         raise credentials_exception
-    user = get_single_user_by_username(session, username=token_data.username)
+    user = get_single_user_by_username(session, token_data.username)
     if user is None:
         raise credentials_exception
     return user
