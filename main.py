9ce81dc5c86b90c5bd7e4bd8a67356f1ce3b6e8f
Philipp <ph.herrmann@schueler.bs1-bt.de>
2025-05-27 11:56:43 +0200

    Removed unused route

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from fastapi import FastAPI
 
 from routers.user import router as user_router
-from routers.token import router as token_router
 
 
 app = FastAPI()
 app.include_router(user_router)
-app.include_router(token_router)
