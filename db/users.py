8821f6346f354989c61e8a51f5125510832e4bb0
Philipp <ph.herrmann@schueler.bs1-bt.de>
2025-05-27 11:47:02 +0200

    #22 Updated users db func

--- db/users.py
+++ db/users.py
@@ -1,6 +1,11 @@
 from sqlmodel import Session, SQLModel, select
 
-from models.user import UserTable, CreateUser, UserInDB
+from models.user import UserTable, CreateUser, UserInDB, UpdateUser
+
+
+def get_all_users_in_db(session: Session) -> list[UserInDB]:
+    users = session.exec(select(UserTable)).all()
+    return [UserInDB.model_validate(user) for user in users]
 
 
 def get_single_user_by_id(session: Session, id: int) -> UserInDB | None:
@@ -20,25 +25,32 @@ def get_single_user_by_username(session: Session, username: str) -> UserInDB | N
 
 def create_user_in_db(session: Session, user: CreateUser) -> UserInDB | None:
     user_table = UserTable.from_create_user(user)
+
     session.add(user_table)
     session.commit()
     session.refresh(user_table)
+
     return UserInDB.model_validate(user_table)
 
 
-def update_user_in_db(session: Session, new_user: CreateUser, id: int) -> SQLModel:
-    old_user = get_single_user_by_id(session, id)
-    if not old_user:
+def update_user_in_db(session: Session, updated_user: UpdateUser, user_id: int) -> UserInDB | None:
+    user_table = session.get(UserTable, user_id)
+
+    if not user_table:
         return None
-    for key, value in new_user.model_dump(exclude_unset=True, exclude_none=True).items():
-        setattr(old_user, key, value)
+    
+    hero_data = updated_user.model_dump(exclude_unset=True, exclude_none=True)
+
+    user_table.sqlmodel_update(hero_data)
+    session.add(user_table)
     session.commit()
-    session.refresh(old_user)
-    return old_user
+    session.refresh(user_table)
+
+    return UserInDB.model_validate(user_table)
 
 
 def delete_user_in_db(session: Session, id: int) -> int:
-    user = get_single_user_by_id(session, id)
+    user = session.get(UserTable, id)
     if not user:
         return None
     session.delete(user)
