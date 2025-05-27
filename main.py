a01b6e29771cb43b1c002e0b520d089db62e246c
Philipp <ph.herrmann@schueler.bs1-bt.de>
2025-05-27 08:39:24 +0200

    #22 Added routes

--- main.py
+++ main.py
@@ -1,64 +1,9 @@
-from typing import Annotated
+from fastapi import FastAPI
 
-from fastapi import FastAPI, Path, HTTPException, status
-from fastapi.responses import  JSONResponse
+from routers.user import router as user_router
+from routers.token import router as token_router
 
-from db import SessionDep, get_single_entity_by_id, create_single_entity_by_id, update_single_entity_by_id, delete_single_entity_by_id
-from models.users import User, CreateUser
-from pydantic import BaseModel
-
-class userIn(BaseModel):
-    username: str
-    email: str
-    alter: int
-    gewicht: float
-    groesse: float
-
-class userOut(BaseModel):
-    id: int
 
 app = FastAPI()
-
-
-@app.post("/user/", response_model=userOut)
-async def create_user(user: CreateUser, session: SessionDep):
-    create_single_entity_by_id(session, User, user)
-    return {"id": 111}
-
-
-@app.put("/user/update/{user_id}", response_model=None)
-async def update_user(user_id: Annotated[ int, Path(title="Benutzer ID", description="ID des Benutzers") ], update_request: CreateUser, session: SessionDep) -> any:
-    return update_user_attributes(user_id, update_request, session)
-
-
-def update_user_attributes(user_id: int, update_request: CreateUser, session: SessionDep) -> any:
-
-    validate_update_numbers(update_request)
-
-    update_single_entity_by_id(session, User, update_request, user_id)
-
-    return JSONResponse(status_code=status.HTTP_200_OK, content={"user_id": user_id})
-
-def validate_update_numbers(update_request: CreateUser):
-
-    if update_request.gewicht is not None:
-        if update_request.gewicht < 0:
-            raise HTTPException(400, "Übergebene Werte dürfen nicht negativ sein!")
-
-    if update_request.groesse is not None:
-        if update_request.groesse < 0:
-            raise HTTPException(400, "Übergebene Werte dürfen nicht negativ sein!")
-
-    if update_request.alter is not None:
-        if update_request.alter < 0:
-            raise HTTPException(400, "Übergebene Werte dürfen nicht negativ sein!")
-            
-
-@app.delete("/user_delete/{user_id}")
-async def user_delete(user_id: int , session: SessionDep):
-
-    deleted = delete_single_entity_by_id(session, User, user_id)
-    if deleted is None: 
-        raise HTTPException(status_code=404, detail=f"User with ID {user_id} not found")
-    
-    return JSONResponse(status_code=204, content={"message": f"User with ID {user_id} deleted successfully."})
+app.include_router(user_router)
+app.include_router(token_router)
