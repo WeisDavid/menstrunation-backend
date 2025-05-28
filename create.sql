a4a6198494be30ecf08f23e226d3d076d5235b0b
Philipp <ph.herrmann@schueler.bs1-bt.de>
2025-05-28 08:09:10 +0200

    Ungenutzte Tabellen entfernt

--- create.sql
+++ create.sql
@@ -8,47 +8,6 @@ CREATE TABLE users(
     height FLOAT
 );
 
-CREATE TABLE products(
-    ID INT PRIMARY KEY AUTO_INCREMENT,
-    name varchar(20),
-    description TEXT
-);
-
-CREATE TABLE vendors(
-	ID INT PRIMARY KEY AUTO_INCREMENT, 
-	name varchar(20), 
-	registerNr INT 
-);
-
-CREATE TABLE ratings(
-     ID INT PRIMARY KEY AUTO_INCREMENT,
-     rating INT, 
-     description VARCHAR(255), 
-     productID INT,
-     userID INT,
-     FOREIGN KEY (userID) REFERENCES users(ID),
-     FOREIGN KEY (productID) REFERENCES products(ID)
- ); 
-
-CREATE TABLE purchases(
-    ID INT PRIMARY KEY AUTO_INCREMENT,
-    userID INT,
-    FOREIGN KEY(userID) REFERENCES users(ID),
-    productID INT,
-    FOREIGN KEY(productID) REFERENCES products(ID),
-    vendorID INT,
-    FOREIGN KEY(vendorID) REFERENCES vendors(ID)
-    );
-
-CREATE TABLE offers(
-    ID int PRIMARY KEY AUTO_INCREMENT,
-    price float,
-    productID int,
-    FOREIGN KEY(productID) REFERENCES products(ID),
-    vendorID int,
-    FOREIGN KEY(vendorID) REFERENCES vendors(ID)
-);
-
 CREATE TABLE buddies(
     ID INT PRIMARY KEY AUTO_INCREMENT,
     userID1 INT,
