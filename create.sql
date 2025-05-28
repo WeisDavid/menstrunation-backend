10526fe61dbe652e80040301bbacaabab9493a2e
Markcggck <ma.rux@schueler.bs1-bt.de>
2025-05-28 08:46:05 +0200

    #25 sql bissl überarbeitet und offers verworfen

--- create.sql
+++ create.sql
@@ -1,67 +1,27 @@
 CREATE TABLE users(
     ID INT PRIMARY KEY AUTO_INCREMENT,
     username varchar(20),
+    email varchar(50),
     password varchar(64),
     age INT,
     weight FLOAT,
     height FLOAT
 );
-
-CREATE TABLE products(
-    ID INT PRIMARY KEY AUTO_INCREMENT,
-    name varchar(20),
-    description TEXT
-);
-
-CREATE TABLE vendors(
-	ID INT PRIMARY KEY AUTO_INCREMENT, 
-	name varchar(20), 
-	registerNr INT 
-);
-
-CREATE TABLE ratings(
-     ID INT PRIMARY KEY AUTO_INCREMENT,
-     rating INT, 
-     description VARCHAR(255), 
-     productID INT,
-     userID INT,
-     FOREIGN KEY (userID) REFERENCES users(ID),
-     FOREIGN KEY (productID) REFERENCES product(ID)
- ); 
-
-CREATE TABLE purchases(
-    ID INT PRIMARY KEY AUTO_INCREMENT,
-    userID INT,
-    FOREIGN KEY(userID) REFERENCES users(ID),
-    productID INT,
-    FOREIGN KEY(productID) REFERENCES products(ID),
-    vendorID INT,
-    FOREIGN KEY(vendorID) REFERENCES vendors(ID)
-    );
-
-CREATE TABLE offers(
-    ID int PRIMARY KEY AUTO_INCREMENT,
-    price float,
-    productID int,
-    FOREIGN KEY(productID) REFERENCES product(ID),
-    vendorID int,
-    FOREIGN KEY(vendorID) REFERENCES vendors(ID),
-);
-
+  
 CREATE TABLE cycles(
     ID INT AUTO_INCREMENT PRIMARY KEY,
     ovulation DATE,
     userID INT,
     FOREIGN KEY (userID) REFERENCES users(ID)
 );
- 
+
 CREATE TABLE fertileDays(
     ID INT AUTO_INCREMENT PRIMARY KEY,
     pregnancy_probability FLOAT,
     cycleID INT,
     FOREIGN KEY (cycleID) REFERENCES cycles(ID)
 );
-
+ 
 CREATE TABLE buddys(
     ID INT PRIMARY KEY AUTO_INCREMENT,
     userID1 INT,
@@ -69,19 +29,20 @@ CREATE TABLE buddys(
     userID2 INT,
     FOREIGN KEY(userID2) REFERENCES users(ID)
     );
-
+ 
 CREATE TABLE chillDays(
   ID INT PRIMARY KEY AUTO_INCREMENT,
   date DATE,
   cycleID INT,
   FOREIGN KEY (cycleID) REFERENCES cycles(ID)
 );
-
-CREATE TABLE periodDays(
+ 
+CREATE TABLE diaryDays(
     ID INT PRIMARY KEY AUTO_INCREMENT,
     painlvl INT,
-    condition TEXT,
+    content TEXT,
+    periodday BOOLEAN,
     date DATE,
     cycleID INT,
     FOREIGN KEY (cycleID) REFERENCES cycles(ID)
-);
+); 
